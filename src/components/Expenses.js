3dd742a71f2c42990fea012febbefee62d296cf5
Matthew Postrel <matthewpostrel@gmail.com>
2023-08-22 10:10:29 -0400

    added dynamic features

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,44 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpenseFilter";
-import './ExpenseFilter.css'
+import "./ExpenseFilter.css";
 
 const Expenses = (props) => {
+  const [fitleredYear, setFilteredYear] = useState("2020");
 
-  const [fitleredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const filterChangeHandler = (selectedYear) => {
-      setFilteredYear(selectedYear);
-    }
-  
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === fitleredYear;
+  });
+
+  let expensesContent = <p className="white">No Expenses Found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div>
       <Card className="expenses">
-
-        <ExpensesFilter selected={fitleredYear} onChangeFilter = {filterChangeHandler}/>
-
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
+        <ExpensesFilter
+          selected={fitleredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expensesContent}
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
